15af660e562bee3599130cfea10e319f996fec55
Srijani Parui <72404928+srijani07@users.noreply.github.com>
2021-07-08 15:34:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "🍏": "Green Apple",
+  "🍋": "Lemon",
+  "😀": "Grinning Face",
+  "😘": "Face Throwing a Kiss",
+  "😍": "Smiling Face With Heart-Eyes",
+  "🐱": "Cat face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we don't have this in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>inside outt!</h1>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2> {meaning} </h2>
+
+      <h3> emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
